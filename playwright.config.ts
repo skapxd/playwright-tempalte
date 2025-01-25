34551ea19a982c69c0cc6a0efdecbdb43de402a8
Manuel Meneses <hbiaser132@gmail.com>
2025-01-25 01:10:24 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,12 +40,13 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  snapshotPathTemplate: '{testDir}/{testFileName}-snapshots/{arg}-{projectName}-{platform}{ext}',
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-
+    video: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -87,9 +88,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {    
+    command: "yarn dev", // O el comando que uses para iniciar tu aplicación
+    url: "http://localhost:3000", // La URL donde se ejecuta tu aplicación
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000, // 120 segundos de timeout para el arranque
+  },
+  },
 });
